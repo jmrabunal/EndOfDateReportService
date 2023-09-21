7ab2508fb9006982d9cb821a1ec44ad6d210252f
timoteo0204 <timoteo0204@gmail.com>
2023-09-21 02:52:10 -0300

    added day total columns

--- EndOfDateReportService/Services/ExcelService.cs
+++ EndOfDateReportService/Services/ExcelService.cs
@@ -92,13 +92,12 @@ public class ExcelService
                 }
             }
         }
-    
 
-}
-    
+
+    }
+
     public async Task ExportToExcel(DateTime fromDateInclusive, DateTime toDateInclusive)
     {
-
         var path = _configuration.GetSection("commisionSalesPath");
         string currentDirectory = Directory.GetCurrentDirectory() + "//" + path.Value;
 
@@ -118,74 +117,108 @@ public class ExcelService
             ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("CommissionSales");
             var brightGreen = System.Drawing.ColorTranslator.FromHtml("#AAFF17");
 
-            for (int i = 0; i < dataTable.Columns.Count; i++)
+            int originalColumnCount = dataTable.Columns.Count;
+
+            int columnIndex = 1;
+            for (int i = 0; i < originalColumnCount; i++)
             {
-                worksheet.Cells[1, i + 1].Value = dataTable.Columns[i].ColumnName;
+                string columnName = dataTable.Columns[i].ColumnName;
 
-                worksheet.Cells[1, i + 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
-                worksheet.Cells[1, i + 1].Style.Fill.BackgroundColor.SetColor(brightGreen);
+                if (columnName.EndsWith("ay"))
+                {
+                    string dayTotalColumnName = columnName + " Total";
+                    worksheet.Cells[1, columnIndex].Value = columnName;
+                    worksheet.Cells[1, columnIndex + 1].Value = dayTotalColumnName;
 
-                if (dataTable.Columns[i].DataType == typeof(DateTime))
-                {
-                    worksheet.Column(i + 1).Style.Numberformat.Format = "yyyy-MM-dd";
-                }
-                else if (dataTable.Columns[i].ColumnName == "Commision Rate")
-                {
                     for (int row = 0; row < dataTable.Rows.Count; row++)
                     {
-                        var cell = worksheet.Cells[row + 2, i + 1];
-                        cell.Style.Numberformat.Format = "0.00\\%"; 
+                        decimal dayQty = Convert.ToDecimal(dataTable.Rows[row][columnName]);
+                        decimal unitPrice = Convert.ToDecimal(dataTable.Rows[row]["PriceSet"]);
+                        decimal dayTotal = dayQty * unitPrice;
 
-                        if (decimal.TryParse(dataTable.Rows[row][i].ToString(), out decimal commissionRate))
-                        {
-                            cell.Value = commissionRate / 100;
-                        }
-                        else
-                        {
-                            cell.Value = dataTable.Rows[row][i]; 
-                        }
+                        worksheet.Cells[row + 2, columnIndex].Value = dayQty;
+                        worksheet.Cells[row + 2, columnIndex + 1].Value = dayTotal;
+                        worksheet.Cells[row + 2, columnIndex].Style.Numberformat.Format = "#,##0.00"; // Format as 7.00
+                        worksheet.Cells[row + 2, columnIndex + 1].Style.Numberformat.Format = "$#,##0.00"; // Format as currency with two decimal places
+
+                        // Set cell size
+                        worksheet.Column(columnIndex).Width = 15;
+                        worksheet.Column(columnIndex + 1).Width = 15;
+
+                        // Set background color
+                        worksheet.Cells[1, columnIndex, 1, columnIndex + 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                        worksheet.Cells[1, columnIndex, 1, columnIndex + 1].Style.Fill.BackgroundColor.SetColor(brightGreen);
                     }
+
+                    columnIndex += 2; // Move to the next column for the next day total
                 }
-                else if (dataTable.Columns[i].ColumnName == "PriceSet")
+                else if (dataTable.Columns[i].DataType == typeof(DateTime))
                 {
+                    worksheet.Cells[1, columnIndex].Value = columnName;
+                    worksheet.Cells[1, columnIndex].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    worksheet.Cells[1, columnIndex].Style.Fill.BackgroundColor.SetColor(brightGreen);
+
                     for (int row = 0; row < dataTable.Rows.Count; row++)
                     {
-                        var cell = worksheet.Cells[row + 2, i + 1];
-                        cell.Style.Numberformat.Format = "$0.0"; 
-                        cell.Value = dataTable.Rows[row][i]; 
+                        worksheet.Cells[row + 2, columnIndex].Value = dataTable.Rows[row][i];
                     }
-                }
-                else if (dataTable.Columns[i].ColumnName.EndsWith("ay")){
-                    for (int row = 0; row < dataTable.Rows.Count; row++)
-                    {
-                        var cell = worksheet.Cells[row + 2, i + 1];
-                        cell.Style.Numberformat.Format = "#,##0.000"; 
-                        cell.Value = dataTable.Rows[row][i];
-                    }
+
+                    worksheet.Column(columnIndex).Width = 15;
+                    worksheet.Column(columnIndex + 1).Width = 15;
+                    worksheet.Column(columnIndex).Style.Numberformat.Format = "yyyy-MM-dd";
+                    
+
+                    columnIndex++;
                 }
                 else
                 {
-                    for (int row = 0; row < dataTable.Rows.Count; row++)
+                    worksheet.Cells[1, columnIndex].Value = columnName;
+                    worksheet.Cells[1, columnIndex].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    worksheet.Cells[1, columnIndex].Style.Fill.BackgroundColor.SetColor(brightGreen);
+
+                    if (columnName == "Commision Rate")
                     {
-                        worksheet.Cells[row + 2, i + 1].Value = dataTable.Rows[row][i];
+                        for (int row = 0; row < dataTable.Rows.Count; row++)
+                        {
+                            var cell = worksheet.Cells[row + 2, columnIndex];
+                            cell.Style.Numberformat.Format = "0%"; // Format as percentage
+
+                            if (decimal.TryParse(dataTable.Rows[row][i].ToString(), out decimal commissionRate))
+                            {
+                                cell.Value = commissionRate / 100;
+                            }
+                            else
+                            {
+                                cell.Value = dataTable.Rows[row][i];
+                            }
+                        }
                     }
-                }
+                    else if (columnName == "PriceSet")
+                    {
+                        for (int row = 0; row < dataTable.Rows.Count; row++)
+                        {
+                            var cell = worksheet.Cells[row + 2, columnIndex];
+                            cell.Style.Numberformat.Format = "$#,##0.00"; // Format as currency with two decimal places
+                            cell.Value = dataTable.Rows[row][i];
+                        }
+                    }
+                    else
+                    {
+                        for (int row = 0; row < dataTable.Rows.Count; row++)
+                        {
+                            worksheet.Cells[row + 2, columnIndex].Value = dataTable.Rows[row][i];
+                        }
+                    }
 
-                worksheet.Column(i + 1).Width = 15;
-            }
-
-
-            for (int row = 0; row < dataTable.Rows.Count; row++)
-            {
-                for (int col = 0; col < dataTable.Columns.Count; col++)
-                {
-                    worksheet.Cells[row + 2, col + 1].Value = dataTable.Rows[row][col];
-
+                    worksheet.Column(columnIndex).Width = 15;
+                    columnIndex++;
                 }
             }
 
             package.Save();
         }
+    
 
-    }
+
+}
 }
